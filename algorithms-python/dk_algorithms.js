127ce8e04b8a0cebbe1ae3afac592b2c44cc02ad
quadri <quadrisyedminnatullah@gmail.com>
2023-10-25 16:28:24 +0530

    Bug fix and finalization of javascript implementaiton of algorithm

--- algorithms-python/dk_algorithms.js
+++ algorithms-python/dk_algorithms.js
@@ -23,8 +23,6 @@ function calculateBestMatch(vec1, vec2, target, p1 = 0, p2 = 0) {
       resCost: curCost,
       resMatch: [vec1.slice(0, p1 + 1), vec2.slice(0, p2 + 1)],
     };
-    //   this.resCost = curCost;
-    //   this.resMatch = [vec1.slice(0, p1 + 1), vec2.slice(0, p2 + 1)];
   }
 
   let agr_res = [];
@@ -126,7 +124,6 @@ function getStdDeviation(dataPoints) {
 function getTemplate(refSigns) {
   // Length tolerance calculation
   const lnRefSigns = refSigns.map((sign) => sign.length);
-  const mean = getMean(lnRefSigns);
   const stdDeviation = getStdDeviation(lnRefSigns);
 
   const minLn = Math.min(...lnRefSigns) - stdDeviation;
@@ -157,97 +154,12 @@ function getTemplate(refSigns) {
   };
 }
 
-class Match {
-  constructor(target) {
-    this.target = target;
-    // this.v1 = vec1;
-    // this.v2 = vec2;
-    // this.exploredVec = [];
-  }
-
-  process(vec1, vec2) {
-    this.resCost = 2 ** 16;
-    this.resMatch = [];
-    // this.calculateBestMatch(vec1, vec2);
-    console.log(this.target);
-    console.log("dd", this.calculateBestMatch(vec1, vec2, this.target));
-    return { resCost: this.resCost, resMatch: this.resMatch };
-  }
-
-  //   calculateBestMatch(vec1, vec2, target, p1 = 0, p2 = 0) {
-  //     // Ignore already completed steps
-  //     const stringified = String([vec1.length, vec2.length, p1, p2]);
-  //     for (const sample of exploredVec) {
-  //       if (String(sample) == stringified) return;
-  //     }
-
-  //     exploredVec.push([vec1.length, vec2.length, p1, p2]);
-
-  //     // Pointers reached at the target
-  //     if (p1 === p2 && p2 === target - 1) {
-  //       const curCost = getCost(vec1.slice(0, p1 + 1), vec2.slice(0, p2 + 1));
-  //       return {
-  //         resCost: curCost,
-  //         resMatch: [vec1.slice(0, p1 + 1), vec2.slice(0, p2 + 1)],
-  //       };
-  //       //   this.resCost = curCost;
-  //       //   this.resMatch = [vec1.slice(0, p1 + 1), vec2.slice(0, p2 + 1)];
-  //     }
-
-  //     let agr_res = [];
-
-  //     // Consider to take from first vector
-  //     if (p1 + 1 < target) {
-  //       let res = this.calculateBestMatch(vec1, vec2, target, p1 + 1, p2);
-  //       if (res) agr_res.push(res);
-  //     }
-
-  //     // Consider to ignore from first vector
-  //     if (vec1.length > target) {
-  //       let res = this.calculateBestMatch(vec1.slice(1), vec2, target, p1, p2);
-  //       if (res) agr_res.push(res);
-  //     }
-
-  //     // Consider to take from second vector
-  //     if (p2 + 1 < target) {
-  //       let res = this.calculateBestMatch(vec1, vec2, target, p1, p2 + 1);
-  //       if (res) agr_res.push(res);
-  //     }
-
-  //     // Consider to ignore from second vector
-  //     if (vec2.length > target) {
-  //       let res = this.calculateBestMatch(vec1, vec2.slice(1), target, p1, p2);
-  //       if (res) agr_res.push(res);
-  //     }
-
-  //     if (agr_res)
-  //       if (agr_res.length != 0) {
-  //         let best = agr_res[0];
-
-  //         for (const i of agr_res) {
-  //           if (i.resCost < best.resCost) best = i;
-  //         }
-
-  //         return best;
-  //       }
-  //   }
-}
-
-function getDetails(v1, v2, tar) {
-  var obj = new Match(tar);
-  console.log(
-    obj.process(
-      [0, -27, -29, -26, -24, -50, -20, 64, 17, 16, 22, 30, 26, -348, 16, 0],
-      [-13, -34, -19, -38, -37, 5, 37, 38, 30, 37]
-    )
-  );
-  var sdfsdafd = new Match(3);
-  console.log(
-    sdfsdafd.process(
-      [0, -27, -29, -26, -24, -50, -20, 64, 17, 16, 22, 30, 26, -348, 16, 0],
-      [-13, -34, -19, -38, -37, 5, 37, 38, 30, 37]
-    )
-  );
+function isValidate(template, input) {
+  // Check if the test sign is valid
+  const x = getBestMatch(template.vector, input, template.lengthMin).resCost;
+  if (template.lengthMin <= input.length <= template.lengthMax)
+    if (template.costMin <= x <= template.costMax) return true;
+  return false;
 }
 
 function main() {
@@ -255,36 +167,25 @@ function main() {
   // Sign: 1: #1 0,-27,-29,-26,-24,-50,-20,64,17,16,22,30,26,-348,16,0
   // Sign: 2: #1 0,354,-16,-26,-27,-16,-50,-35,18,43,55,26,27,-343,41,0
   // Sign: 3: #1 0,-42,-19,-26,-47,-32,43,40,36,45,-339,0
-  const sn = [];
-  sn.push([
+  let s1 = [
     0, -27, -29, -26, -24, -50, -20, 64, 17, 16, 22, 30, 26, -348, 16, 0,
-  ]);
-  sn.push([
+  ];
+  let s2 = [
     0, 354, -16, -26, -27, -16, -50, -35, 18, 43, 55, 26, 27, -343, 41, 0,
-  ]);
-  sn.push([0, -42, -19, -26, -47, -32, 43, 40, 36, 45, -339, 0]);
+  ];
+  let s3 = [0, -42, -19, -26, -47, -32, 43, 40, 36, 45, -339, 0];
 
   // Get the template for the given reference signs
-  const template = getTemplate(sn);
+  const template = getTemplate([s1, s2, s3]);
   console.log("got template", template);
 
   // Test the template on a new sign
   const testVec = [
     0, -47, -19, -39, -38, -33, 19, 26, 52, 37, 22, 17, -342, 17, 0,
   ];
-  const x = getBestMatch(template.vector, testVec, template.lengthMin).resCost;
 
-  // Check if the test sign is valid
-  if (template.lengthMin <= testVec.length <= template.lengthMax) {
-    if (template.costMin <= x <= template.costMax) {
-      console.log("Valid Signature!");
-    } else {
-      console.log("Invalid cost!");
-    }
-  } else {
-    console.log("Invalid length!");
-  }
+  if (isValidate(template, testVec)) console.log("Valid Signature!");
+  else console.log("Invalid!");
 }
-
 // Call the main function
-main();
+// main();
