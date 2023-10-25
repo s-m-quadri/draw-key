4c75e00e3be12ebe25d3ca044c87ed688e8c74c3
quadri <quadrisyedminnatullah@gmail.com>
2023-10-25 11:24:44 +0530

    Commented Algorithm and removed useful files

--- algorithms-python/run.sh
+++ algorithms-python/run.sh
@@ -1,3 +1,13 @@
-echo "Running Algorithms"
-pytest test_dk_algorithms.py
+echo Running Algorithms
+
+echo '=====> Step 1/3 Installing Requrements...'
+# python3 -m pip install -r requirements.txt > _temp
+
+echo '=====> Step 2/3 Checking Types...'
+python3 -m mypy dk_algorithms.py
+
+echo '=====> Step 3/3 Running Tests...'
+python3 -m pytest test_dk_algorithms.py
+
+echo '=====> Now, Running Program...'
 python3 dk_algorithms.py
